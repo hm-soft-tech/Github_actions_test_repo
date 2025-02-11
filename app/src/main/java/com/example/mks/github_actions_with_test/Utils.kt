b9f693f62890f09d6c78eb23b22f537c4d8de3ee
hp <hamzamalik96atd@gmail.com>
2025-02-11 22:38:14 +0500

    first commit

---
+++ app/src/main/java/com/example/mks/github_actions_with_test/Utils.kt
@@ -0,0 +1,19 @@
+package com.example.mks.github_actions_with_test
+
+class Utils {
+
+
+  fun addition(a:Int,b:Int):Boolean{
+    val  sum  = a + b
+      if (sum > 40)
+      {
+          return true
+      }
+      else{
+          return false
+      }
+
+  }
+
+
+}
---
+++ app/src/test/java/com/example/mks/github_actions_with_test/UtilsTest.kt
@@ -0,0 +1,32 @@
+package com.example.mks.github_actions_with_test
+
+import org.junit.Assert
+import org.junit.Test
+import org.junit.jupiter.api.Assertions.*
+
+class UtilsTest {
+    lateinit var test:Utils
+
+    @org.junit.jupiter.api.BeforeEach
+    fun setUp() {
+
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    fun tearDown() {
+    }
+
+    @Test
+    fun checksum(){
+        test  = Utils()
+    val result =  test.addition(2,7)
+     Assert.assertFalse(result)
+
+    // Assert.assertEquals(true,true)
+
+
+    }
+
+
+
+}
